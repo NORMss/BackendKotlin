eca9a790ded0c67905265b94bc63a2887dd93aeb
NORMss <serega.b301@gmail.com>
2025-04-12 23:58:07 +0700

    feat: created token storage in the database

--- src/main/kotlin/ru/normno/backendcourse/spring_boot_course/security/AuthService.kt
+++ src/main/kotlin/ru/normno/backendcourse/spring_boot_course/security/AuthService.kt
@@ -1,15 +1,22 @@
 package ru.normno.backendcourse.spring_boot_course.security
 
+import org.bson.types.ObjectId
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Service
+import ru.normno.backendcourse.spring_boot_course.database.model.RefreshToken
 import ru.normno.backendcourse.spring_boot_course.database.model.User
+import ru.normno.backendcourse.spring_boot_course.database.repository.RefreshTokenRepository
 import ru.normno.backendcourse.spring_boot_course.database.repository.UserRepository
+import java.security.MessageDigest
+import java.time.Instant
+import java.util.*
 
 @Service
 class AuthService(
     private val jwtService: JwtService,
     private val userRepository: UserRepository,
-    private val hashEncoder: HashEncoder
+    private val hashEncoder: HashEncoder,
+    private val refreshTokenRepository: RefreshTokenRepository,
 ) {
     data class TokenPair(
         val accessToken: String,
@@ -35,9 +42,34 @@ class AuthService(
         val newAccessToken = jwtService.generateAccessToken(user.id.toHexString())
         val newRefreshToken = jwtService.generateRefreshToken(user.id.toHexString())
 
+        storeRefreshToken(
+            userId = user.id,
+            rawRefreshToken = newRefreshToken,
+        )
+
         return TokenPair(
             accessToken = newAccessToken,
             refreshToken = newRefreshToken,
         )
+    }
+
+    private fun storeRefreshToken(userId: ObjectId, rawRefreshToken: String) {
+        val hashed = hashToken(rawRefreshToken)
+        val expiryMs = jwtService.refreshTokenValidityMs
+        val expiresAt = Instant.now().plusMillis(expiryMs)
+
+        refreshTokenRepository.save(
+            RefreshToken(
+                userId = userId,
+                expiresAt = expiresAt,
+                hashedToken = hashed,
+            )
+        )
+    }
+
+    private fun hashToken(token: String): String {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashedBytes = digest.digest(token.encodeToByteArray())
+        return Base64.getEncoder().encodeToString(hashedBytes)
     }
 }
